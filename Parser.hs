56f6618d4eaa8ed863447228706abb55166a8d03
willem obbens <willemobbens@gmail.com>
2012-11-10 15:06:19 +0000

    added some more parsers

--- Parser.hs
+++ Parser.hs
@@ -2,19 +2,34 @@
 
 module Parser where
 
-import Control.Applicative ((<$>), (*>), (<*>))
+import Control.Applicative ((<$>), (*>), (<*), (<*>))
 import Data.Maybe (fromJust)
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (char)
+import qualified Text.ParserCombinators.Parsec as P
 
 import Ast
 
+char :: Parser LispVal
+char = Char <$> (P.string "#\\" *> letter)
+
 string :: Parser LispVal
 string = String <$> between doubleQuote doubleQuote (many (escaped <|> noneOf "\"\\"))
-  where doubleQuote = char '"'
-        escaped = char '\\'
-                *> (fromJust
-               <$> flip lookup specialChars
-               <$> oneOf "\\\"nrt")
+  where doubleQuote = P.char '"'
+        escaped = P.char '\\'
+               *> (fromJust <$> flip lookup specialChars
+                            <$> oneOf "\\\"nrt")
         specialChars = [('\\', '\\'), ('"', '"'), ('n', '\n'),
                         ('r', '\r'), ('t', '\t')]
+
+symbol :: Parser LispVal
+symbol = Symbol <$> ((:) <$> letter <*> many (letter <|> digit))
+
+number :: Parser LispVal
+number = Number . read <$> many1 digit
+
+float :: Parser LispVal
+float = Float
+    <$> ((+) <$> (read <$> many digit)
+             <*  P.char '.'
+              *> (read . ("0." ++) <$> many1 digit))
                   
