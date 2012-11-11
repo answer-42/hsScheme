51762dd5e5cb052db2d503f86904cbc138ce57e8
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-11 17:18:37 +0100

    modified:   Main.hs 	modified:   TransformTopDefinitions.hs

--- Main.hs
+++ Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
 import AST
+import Parser
 import InternalDefinitions
 import TransformTopDefinitions
 import ApplyMacros
 
-mainTest input = (removeIntDef . applyMacros . transformTopDef) input
+mainTest input = case readExpr input of
+                  Right ast -> show $ (removeIntDef . applyMacros . transformTopDef) ast
+                  Left err  -> show err
--- TransformTopDefinitions.hs
+++ TransformTopDefinitions.hs
@@ -2,4 +2,11 @@ module TransformTopDefinitions where
 
 import AST
 
-transformTopDef = id
+{- TODO add some more cases -}
+
+transformTopDef :: [LispVal] -> [LispVal]
+transformTopDef = map transform
+  where transDefLam x xs = List [Symbol "define", head x, List $ [Symbol "lambda", List $ tail x] ++ xs]   
+        transform l@(List x) = case x of
+                                (Symbol "define"):(List y):ys -> transDefLam y ys 
+                                _ -> l
