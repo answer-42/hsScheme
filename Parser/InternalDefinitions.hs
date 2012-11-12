511162ec4e4fada61d3eeb257664fe37f03309bd
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-12 20:06:26 +0100

    Admit defeat :D

--- Parser/InternalDefinitions.hs
+++ Parser/InternalDefinitions.hs
@@ -9,23 +9,27 @@ import Parser.AST
  - See r5rs 5.2.2, also for toplevel let expressions.
  -}
 
+{- TODO rewrite!! -}
+
 removeIntDef :: [LispVal] -> [LispVal]
 removeIntDef = map transIntDef 
   where removeDef :: [LispVal] -> LispVal -> LispVal
-        removeDef d (List (Symbol "lambda":x:xs)) =
-            List $ d ++ [List $ [Symbol "lambda", x] ++ changeDef xs]
+        -- removeDef d (List (Symbol "lambda":x:xs)) =
+        --     List $ d ++ [List $ [Symbol "lambda", x] ++ changeDef xs]
+        removeDef d xs = List $ d -- ++ changeDef [xs]
 
         changeDef xs = 
             [List $ [Symbol "letrec", 
                      List $ map (\y -> List [fst y, snd y]) defines] ++ rest]
             where defines = map (\y -> case y of 
-                                         List (Symbol "define":a:as:[]) -> (a,as)) $
+                                         List [Symbol "define",a,as] -> (a,as)) $
                             filter isDefine xs
                   rest = filter (not . isDefine) xs
 
         transIntDef :: LispVal -> LispVal
+        -- This doesn't math a let expression...Why not??
         transIntDef (List [Symbol "let",x,xs]) =
-            removeDef [Symbol "let",x] xs
+            removeDef [Symbol "let"] xs
         transIntDef (List [Symbol "define",x,xs]) = 
             removeDef [Symbol "define",x] xs
         transIntDef r = r
--- Test/InternalDefinitions.hs
+++ Test/InternalDefinitions.hs
@@ -7,8 +7,8 @@ import Parser.InternalDefinitions
 tests = [  ("(define x (lambda (y) (define a b) (+ 11 2) a))",
            "[(define x (lambda (y) (letrec ((a b)) (+ 11 2) a)))]")
          ,("1", "[1]")
-         ,("(let ((x y)) (define a b) a)",
-           "[(let ((x y)) (letrec ((a b)) a))]")
+         ,("(let ((x y) (z w))  (define a b) a)",
+           "[(let ((x y) (z w)) (letrec ((a b)) a))]")
         ]
 
 testReadExpr :: (String, String) -> String
