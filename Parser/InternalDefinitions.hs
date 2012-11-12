5f1670bf054f563bdaf2ec9617fceb8ef960ef1a
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-12 13:31:22 +0100

    Some small changes.

--- Parser/InternalDefinitions.hs
+++ Parser/InternalDefinitions.hs
@@ -27,4 +27,5 @@ removeIntDef = map transIntDef
 
         transIntDef :: LispVal -> LispVal
         transIntDef (List (Symbol "define":x:xs:[])) = removeDef (Symbol "define":x:[]) xs
+--        transIntDef (List (Symbol "let":x:xs:[])) = removeDef (Symbol "let":x:[]) xs
         transIntDef r = r
