9fe90943bc875b9401d4513ea6c828f8afdca42a
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-12 13:21:13 +0100

    Internal Transformation.

--- Parser/InternalDefinitions.hs
+++ Parser/InternalDefinitions.hs
@@ -1,20 +1,30 @@
 module Parser.InternalDefinitions where
 
 import Parser.AST
-import Data.List (foldr)
+-- import Data.List (foldr)
+import Control.Applicative
 
 {- TODO
  - Transform internal definitions to letrec definitions.
- - See r5rs 5.2.2
+ - See r5rs 5.2.2, also for toplevel let expressions.
  -}
 
 removeIntDef :: [LispVal] -> [LispVal]
 removeIntDef = map transIntDef 
-  where removeDef :: [LispVal] -> [LispVal] -> LispVal
-        removeDef d xs = List $ (d++xs) --List $ foldr (\a b -> ) 
-        
-        transIntDef :: LispVal -> LispVal
-        transIntDef (List (Symbol "define":x:xs:xss)) = removeDef (Symbol "define":x:xs:[]) xss
-        transIntDef r = r
+  where removeDef :: [LispVal] -> LispVal -> LispVal
+        removeDef d (List (Symbol "lambda":x:xs)) =
+            List $ d++[List $ [Symbol "lambda", x]++(changeDef xs)]
 
--- removeIntDef = id
+        changeDef xs = 
+            [List $ [Symbol "letrec", 
+                     List $ List <$> map (\y -> [fst y, snd y ]) defines] ++ rest]
+            where isDefine (List (Symbol "define":xs)) = True
+                  isDefine _ = False
+                               
+                  defines = map (\y -> case y of (List (Symbol "define":a:as:[])) -> (a,as)) $
+                            filter isDefine xs
+                  rest = filter (not . isDefine) xs
+
+        transIntDef :: LispVal -> LispVal
+        transIntDef (List (Symbol "define":x:xs:[])) = removeDef (Symbol "define":x:[]) xs
+        transIntDef r = r
