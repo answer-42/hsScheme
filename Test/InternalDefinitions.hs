89688b7c717e35a692fd36ea319f0da6ead2cbed
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-12 17:49:19 +0100

    Tests InternalDefinition

---
+++ Test/InternalDefinitions.hs
@@ -0,0 +1,18 @@
+module Test.InteralDefinitions where
+
+import Parser.AST
+import Parser.Parser
+import Parser.InternalDefinitions
+
+tests = [ ("(define x (lambda (y) (define a b) a))",
+          "[(define x (lambda (y) (letrec ((a b)) a)))]")
+        ]
+
+testReadExpr :: (String, String) -> String
+testReadExpr s = case readExpr $ fst s of
+                   Right exp -> if ((show . removeIntDef) exp == snd s) 
+                                then show "Success" 
+                                else (show . removeIntDef) exp
+                   Left e -> show e 
+
+test = map testReadExpr tests
