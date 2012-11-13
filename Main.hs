0b8390fb563e28fc254ea6391b81d47965b65665
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-13 14:43:42 +0100

    Toedoe

--- Main.hs
+++ Main.hs
@@ -6,11 +6,13 @@ import Parser.InternalDefinitions
 import Parser.TransformTopDefinitions
 import Parser.ApplyMacros
 import Parser.IfToCond
+import Parser.LetStarToLet
 
 mainTest input = case readExpr input of
                    Right ast -> show $ 
-                                (--ifTrans . 
-                                 --applyMacros . 
+                                (letStarTrans .
+                                 ifTrans . 
+                                 applyMacros . 
                                  removeIntDef .
                                  transformTopDef
                                 ) ast
