3ac4e572604b88acc85e515cca7052c0703defb1
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-14 11:29:56 +0100

    Some work on letToLambda.

--- Parser/LetToLambda.hs
+++ Parser/LetToLambda.hs
@@ -5,6 +5,17 @@ import Parser.AST
 letToLambda :: AST -> AST
 letToLambda = map changeLet
     where changeLet :: LispVal -> LispVal
-          -- changeLet (List [Symbol "let",x,xs]) =
-          changeLet = id
+          -- Normal Let
+          changeLet (List [Symbol "let",List x,xs]) = 
+              List $ [List [sLam, List $ map getSym x, xs]] ++ (map getArg x)
+
+          -- TODO Named Let, recursive
+          changeLet r = r
+
+          getSym :: LispVal -> LispVal
+          getSym (List x) = head x
+
+          getArg :: LispVal -> LispVal
+          getArg (List x) = (head . tail) x
+
               
