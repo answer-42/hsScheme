bc9bd195488c8597abcff17e08bd317b27066e1e
Sebastian Benque <sebastian.benque@gmail.com>
2012-11-14 12:28:06 +0100

    letToLambda seems to work

--- Parser/LetToLambda.hs
+++ Parser/LetToLambda.hs
@@ -5,17 +5,19 @@ import Parser.AST
 letToLambda :: AST -> AST
 letToLambda = map changeLet
     where changeLet :: LispVal -> LispVal
-          -- Normal Let
           changeLet (List [Symbol "let",List x,xs]) = 
-              List $ [List [sLam, List $ map getSym x, xs]] ++ (map getArg x)
-
-          -- TODO Named Let, recursive
+              let xs' = (case xs of 
+                          (List y) -> y
+                          y -> [y])
+              in 
+              List $ [List $ [sLam,List $ map getSym x] ++ (letToLambda xs')] ++ (map getArg x)
+          changeLet (List x) = List $ letToLambda x
           changeLet r = r
 
           getSym :: LispVal -> LispVal
-          getSym (List x) = head x
+          getSym (List x) = changeLet $ head x
 
           getArg :: LispVal -> LispVal
-          getArg (List x) = (head . tail) x
+          getArg (List x) = changeLet $ (head . tail) x
 
               
