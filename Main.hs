b83cf082d20b6b9785cdb44fdbfdc2b7e04af1eb
Sebastian Benque <sebastian.benque@gmail.com>
2012-12-07 17:41:26 +0100

    Removed some useless files..

--- Main.hs
+++ Main.hs
@@ -2,13 +2,9 @@ module Main where
 
 import Parser.AST
 import Parser.Parser
-import Parser.InternalDefinitions
-import Parser.TransformTopDefinitions
+import Parser.Transformations
 import Parser.ApplyMacros
-import Parser.IfToCond
-import Parser.LetStarToLet
-import Parser.LetToLambda
-import Parser.AndOrToIf
+
 
 mainTest input = case readExpr input of
                    Right ast -> show $ 
