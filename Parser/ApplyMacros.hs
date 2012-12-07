b21b7fafa79f5ce3e626c382f1e98f9bfd334a95
Sebastian Benque <sebastian.benque@gmail.com>
2012-12-07 20:10:12 +0100

    Data type for macros. Scribbled some ideas down.

--- Parser/ApplyMacros.hs
+++ Parser/ApplyMacros.hs
@@ -2,7 +2,27 @@ module Parser.ApplyMacros where
 
 import Parser.AST
 
-applyMacros = id
+{- Datatype where we can match the keyword in our AST tree and then transform it
+ - following the transformer rules. Each transformer rule has its own pattern
+ - to match against. 
+ -}
+data Macro = Macro { keyword :: LispVal
+                    ,transformers :: [LispVal] 
+                   } deriving (Show)
+
+{- Standard macros from the R5RS. 
+ -}
+macros = [
+  
+]
+
+{- TODO:
+ - We have to implement a macro reader that finds all syntax rules and adds them
+ - to the list of all known macros. It also has to return the new AST without
+ - the syntax rules!
+ -}
+
+applyMacros = id 
 
 {- TODO
  - Apply macros to AST
