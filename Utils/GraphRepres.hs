b90ff67a5d9a28ce4479bc6a8feb8fabf1ad4c0e
Sebastian Benque <sebastian.benque@gmail.com>
2013-01-26 20:24:23 +0100

    More refactoring.

--- Utils/GraphRepres.hs
+++ Utils/GraphRepres.hs
@@ -10,32 +10,35 @@ import Parser.Parser
 -- Given an AST, create a graph G, and then represent it in Graphviz
 
 {- TODO
-- * Change names
 - * Proper testing
 - * graphical representation
+- * Add in astToGraph proper support for List, so that a List gets a top Node.
   -}
 
-graph = mkGraph [(0, "TOP")] [] :: Gr String Int
-
+graph = mkGraph [(0, "TOP")] [] :: Gr String ()
 
 -- Get the node with the highest node number, where Node Numbers are positive
 -- integers.
 getLastNode :: [(Node, a)] -> Node
 getLastNode = foldr (\(e, _) a -> if e > a then e else a) 0
 
--- astToGraph :: AST -> Gr String Int 
-astToGraph = foldl (\g e -> let pre = getLastNode $ labNodes g
-                            in addNode g pre e 0)
-                       graph 
-  where addNode g pre (List x) base = addListNode g pre base x
-        -- TODO add support for dotted list
-        addNode g pre x base      = insEdge (base, pre+1, -1) $ insNode (pre+1, show x) g
+-- Insert a Node with an edge to the source node (singular)
+insNodeEdge :: Node -> Node -> a -> b -> Gr a b -> Gr a b
+insNodeEdge s t n w g = insEdge (s, t, w) $ insNode (t, n) g 
 
-        addListNode g pre preN [] = addNode g pre (String "NIL" :: LispVal) preN
-        addListNode g pre preN (x:xs) =
-          let g' = addNode g pre x preN
-              pre' = getLastNode $ labNodes g'
-          in addListNode g'  pre' pre' xs
+astToGraph :: AST -> Gr String () 
+astToGraph = foldl (\g e -> let l = getLastNode $ labNodes g
+                            in addNode g l e 0)
+                       graph 
+  where addNode g l (List x) s = addListNode g l s x
+        -- TODO add support for dotted list
+        addNode g l x s        = insNodeEdge s (l+1) (show x) () g 
+
+        addListNode g l s [] = addNode g l (String "NIL" :: LispVal) s
+        addListNode g l s (x:xs) =
+          let g'  = addNode g l x s
+              l'  = getLastNode $ labNodes g'
+          in addListNode g' l' l' xs
 
 mkASTGraph :: String -> String
 mkASTGraph input = case readExpr input of
