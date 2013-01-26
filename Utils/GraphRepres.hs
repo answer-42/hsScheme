6d391da200d7b8a9b91b4c916083667632ec86eb
Sebastian Benque <sebastian.benque@gmail.com>
2013-01-26 17:05:12 +0100

    Small stylish changes, TODO

--- Utils/GraphRepres.hs
+++ Utils/GraphRepres.hs
@@ -9,14 +9,19 @@ import Parser.Parser
 
 -- Given an AST, create a graph G, and then represent it in Graphviz
 
+{- TODO
+- * Change names
+- * Proper testing
+- * graphical representation
+  -}
+
 graph = mkGraph [(0, "TOP")] [] :: Gr String Int
 
 -- astToGraph :: AST -> Gr String Int 
-astToGraph ast = foldl (\g e -> let ns = labNodes g
+astToGraph = foldl (\g e -> let ns = labNodes g
                                     pre = foldr (\(e,_) a -> if e>a then e else a) 0 ns
-                                in addNode g pre e 0)
-                                --insEdge (0,pre+1, -1) (insNode (pre+1,show e) a))
-                       graph ast
+                            in addNode g pre e 0)
+                       graph 
   where addNode g pre (List x) base = addListNode g pre base x
         -- TODO add support for dotted list
         addNode g pre x base      = insEdge (base, pre+1, -1) $ insNode (pre+1, show x) g
@@ -25,7 +30,7 @@ astToGraph ast = foldl (\g e -> let ns = labNodes g
         addListNode g pre preN (x:xs) =
           let g' = addNode g pre x preN
               pre' = foldr (\(e,_) a -> if e>a then e else a) 0 $ labNodes g'
-          in addListNode g'  pre' (pre') xs
+          in addListNode g'  pre' pre' xs
 
 mkASTGraph :: String -> String
 mkASTGraph input = case readExpr input of
