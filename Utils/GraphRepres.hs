0b0cb61a4f1fbe9bf21a9b9125905794b9516b87
Sebastian Benque <sebastian.benque@gmail.com>
2013-01-26 20:08:37 +0100

    Some refactoring.

--- Utils/GraphRepres.hs
+++ Utils/GraphRepres.hs
@@ -17,9 +17,14 @@ import Parser.Parser
 
 graph = mkGraph [(0, "TOP")] [] :: Gr String Int
 
+
+-- Get the node with the highest node number, where Node Numbers are positive
+-- integers.
+getLastNode :: [(Node, a)] -> Node
+getLastNode = foldr (\(e, _) a -> if e > a then e else a) 0
+
 -- astToGraph :: AST -> Gr String Int 
-astToGraph = foldl (\g e -> let ns = labNodes g
-                                pre = foldr (\(e,_) a -> if e>a then e else a) 0 ns
+astToGraph = foldl (\g e -> let pre = getLastNode $ labNodes g
                             in addNode g pre e 0)
                        graph 
   where addNode g pre (List x) base = addListNode g pre base x
@@ -29,7 +34,7 @@ astToGraph = foldl (\g e -> let ns = labNodes g
         addListNode g pre preN [] = addNode g pre (String "NIL" :: LispVal) preN
         addListNode g pre preN (x:xs) =
           let g' = addNode g pre x preN
-              pre' = foldr (\(e,_) a -> if e>a then e else a) 0 $ labNodes g'
+              pre' = getLastNode $ labNodes g'
           in addListNode g'  pre' pre' xs
 
 mkASTGraph :: String -> String
