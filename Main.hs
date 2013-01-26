9180935e1ee6f52b117373f5aa52f9881238e92e
Sebastian Benque <sebastian.benque@gmail.com>
2013-01-26 13:01:36 +0100

    Added a main function, so it is easier to use it.

--- Main.hs
+++ Main.hs
@@ -1,18 +1,31 @@
 module Main where
 
+import System.Environment (getArgs)
+import Control.Monad (unless, void)
+
 import Parser.AST
 import Parser.Parser
 import Parser.Transformations
 import Parser.ApplyMacros
 
+import Test.TestMain
 
-mainTest input = case readExpr input of
-                   Right ast -> show $ 
-                                (letToLambda .
-                                 letStarTrans .
-                                 ifTrans . 
-                                 applyMacros . 
-                                 removeIntDef .
-                                 transformTopDef
-                                ) ast
-                   Left err  -> show err
+main = do
+  args <- getArgs
+  unless (null args)
+         (case head args of
+               "-t"      -> void doTests
+               -- Insert new commands here
+               otherwise -> void doUsage)
+  where doTests = test
+        doUsage = putStrLn "TODO: Usage text"
+
+compile input = case readExpr input of
+                     Right ast -> let x      = (removeIntDef . transformTopDef) ast
+                                      macros = readMacros x
+                                      x'     = removeMacros x
+                                  in show $ (letToLambda . 
+                                      letStarTrans . 
+                                      ifTrans . 
+                                      applyMacros macros) x'
+                     Left err  -> show err
